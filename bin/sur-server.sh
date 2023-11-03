e681567c69fe6af752c1085e14f1f6ee918d14d3
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-03 17:11:40 +0100

    chore (dev): Always set RUST_BACKTRACE var

--- bin/sur-server.sh
+++ bin/sur-server.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+export RUST_BACKTRACE=1
 # surreal start --auth --user root --pass root --bind 0.0.0.0:8000 memory
 # surreal start --auth --user root --pass root --bind 0.0.0.0:8000 --allow-scripting  --allow-funcs --allow-net memory
 
-# --log trace \
+# export RUST_BACKTRACE=1
+# # --log trace \
 surreal start \
     --auth \
     --user root \
