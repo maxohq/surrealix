6ba84e93b0c0ec24f6a9e0deff986f37c0167759
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-06 21:50:17 +0100

    chore: Make codegen invocation resistent to the folder the script is called from

--- gen/index.ts
+++ gen/index.ts
@@ -3,8 +3,8 @@ import { TopGenerator } from "./src/TopGenerator";
 
 const socketGen = new SocketGenerator();
 socketGen.run();
-await Bun.write("../lib/socket.ex", socketGen.content);
+await Bun.write(import.meta.dir + "/../lib/surrealix/socket.ex", socketGen.content);
 
 const topGen = new TopGenerator();
 topGen.run();
-await Bun.write("../lib/surrealix.ex", topGen.content);
+await Bun.write(import.meta.dir + "/../lib/surrealix.ex", topGen.content);
