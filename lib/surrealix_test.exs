af04d92887525dd126485a4d88fd44bed68ab4fb
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-07 20:57:25 +0100

    Chore: better names for tests

--- lib/surrealix_test.exs
+++ lib/surrealix_test.exs
@@ -2,8 +2,8 @@ defmodule Surrealix.Test do
   use ExUnit.Case
   import TestSupport
 
-  describe "basic case" do
-    test "works" do
+  describe "basic API" do
+    test "insert / query" do
       with_cleanup(fn pid ->
         Surrealix.insert(pid, "user", %{id: "marcus", name: "Marcus Aurelius"})
         res = Surrealix.query(pid, "select * from user:marcus") |> extract_res(0)
