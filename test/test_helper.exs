59faa8f860be48fe7456d3eebc2f0908b53bfe3c
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-07 21:20:35 +0100

    Fix: better random DB name generator

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -9,18 +9,25 @@ defmodule TestSupport do
     extract_res({:ok, res}) |> Enum.at(index) |> Map.get("result")
   end
 
-  def db_name(), do: for(_ <- 1..10, into: "", do: <<Enum.random(~c"0123456789abcdef")>>)
+  def db_name() do
+    rand =
+      :crypto.strong_rand_bytes(6)
+      |> Base.encode64()
+
+    "test_#{Regex.replace(~r/\W/, rand, "")}"
+  end
 
   def with_cleanup(fun) do
     db = db_name()
+    # IO.puts("*********** DB: #{db}")
     {:ok, pid} = Surrealix.start_link()
     Surrealix.signin(pid, %{user: "root", pass: "root"})
     Surrealix.use(pid, "test", db)
 
     fun.(pid)
 
-    res = Surrealix.query(pid, "remove database #{db};")
-    IO.inspect(res, label: :res)
+    _res = Surrealix.query(pid, "remove database #{db};")
+    # IO.inspect(res, label: :res)
     Surrealix.stop(pid)
   end
 end
