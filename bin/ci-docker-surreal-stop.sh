c1f5a627bc5ede6d5c00e2994628d7ca403f9af8
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-08 21:35:07 +0100

    Feat: Add github CI

---
+++ bin/ci-docker-surreal-stop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+WORKING_DIR=$(dirname "$0")
+CONTAINER_NAME="surrealdb-vecufy-e2e"
+DATA_PATH="${PWD}/.test_data/$CONTAINER_NAME"
+
+echo " "
+echo "Stopping SurrealDB container"
+echo "-----------------------------------------"
+echo "WORKING_DIR=$WORKING_DIR"
+echo "CONTAINER_NAME=$CONTAINER_NAME"
+echo "DATA_PATH=$DATA_PATH"
+echo "-----------------------------------------"
+echo " "
+
+docker stop "$CONTAINER_NAME"
+echo "Removing container"
+docker rm "$CONTAINER_NAME"
+
+echo " "
+echo "Container is down!"
