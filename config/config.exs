961b48dd47c952b2ddc7a2bd0425de6e11e2a64c
Roman Heinrich <roman.heinrich@gmail.com>
2023-11-12 17:50:16 +0100

    Chore: cleanup config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,12 @@
 import Config
 
-config :logger, :console,
-  format: "[$level] $message\n",
-  # level: :debug
-
-  level: :warning
+config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, level: :warning
+# config :logger, :console, level: :debug
 
 config :surrealix, :timeout, 5000
+config :surrealix, :backoff_max, 2000
+config :surrealix, :backoff_step, 100
 
 config :surrealix, :conn,
   hostname: "0.0.0.0",
